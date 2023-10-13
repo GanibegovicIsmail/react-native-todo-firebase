97875118f6e0d0766ca8f5d1d2d7609c4ce0d58f
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-13 21:36:12 +0200

    fix: navigation from list to details

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import List from "./app/screens/List";
+import Details from "./app/screens/Details";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,16 +10,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="My Todos" component={List} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const List = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>List</Text>
+      <Button
+        onPress={() => navigation.navigate("Details")}
+        title="Open Details"
+      />
     </View>
   );
 };
