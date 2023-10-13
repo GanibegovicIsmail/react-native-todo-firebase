dc8ec30013263e7708ea36162e2d67106127388d
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-13 22:12:12 +0200

    feat: todo btn

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,17 +1,23 @@
 import { Text, View, Button } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { FIRESTORE_DB } from "../../firebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
 
 const List = () => {
   const navigation = useNavigation();
 
+  const addTodo = async () => {
+    const doc = addDoc(collection(FIRESTORE_DB, "todos"), {
+      title: "I am a test",
+      done: false,
+    });
+    console.log("~ file: List.tsx:12 ~ addTodo ~doc:", doc);
+  };
+
   return (
     <View>
-      <Text>List</Text>
-      <Button
-        onPress={() => navigation.navigate("Details")}
-        title="Open Details"
-      />
+      <Button onPress={() => addTodo} title="Add Todo" />
     </View>
   );
 };
