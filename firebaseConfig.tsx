804d8c6f690b0573e314a55292709cae5f5cb707
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-13 21:01:24 +0200

    git init

---
+++ firebaseConfig.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBR2tXIi6QVeNo-KNHge2f4HUGs5YTztdw",
+  authDomain: "firestreamapp-2feb3.firebaseapp.com",
+  projectId: "firestreamapp-2feb3",
+  storageBucket: "firestreamapp-2feb3.appspot.com",
+  messagingSenderId: "577657498955",
+  appId: "1:577657498955:web:07d08381b07d81a0474cce",
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
