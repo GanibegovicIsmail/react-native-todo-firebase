3924e3e2a4eb149570a1512741ae232ba1e33d87
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-14 18:48:49 +0200

    fix: app screen changes

--- App.tsx
+++ App.tsx
@@ -3,13 +3,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import List from "./app/screens/List";
 import Details from "./app/screens/Details";
 import Login from "./app/screens/Login";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged;
+  });
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="List">
         <Stack.Screen name="My Todos" component={List} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
