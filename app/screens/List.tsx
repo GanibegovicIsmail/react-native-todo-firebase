4793496cfab9bf8ffc609d7b0c81017a7f9d5d3a
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-14 12:20:39 +0200

    feat: displaying todos

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -2,13 +2,33 @@ import { Text, View, Button, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { FIRESTORE_DB } from "../../firebaseConfig";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { TextInput } from "react-native";
 
 const List = () => {
   const [todos, setTodos] = useState<any[]>([]);
   const [todo, setTodo] = useState("");
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const todoRef = collection(FIRESTORE_DB, "todos");
+
+    const subsriber = onSnapshot(todoRef, {
+      next: (snapshot) => {
+        const todos: any[] = [];
+        snapshot.docs.forEach((doc) => {
+          console.log("UPDATED");
+          todos.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        setTodos(todos);
+      },
+    });
+
+    return () => subsriber();
+  }, []);
 
   const addTodo = async () => {
     const doc = await addDoc(collection(FIRESTORE_DB, "todos"), {
@@ -33,6 +53,11 @@ const List = () => {
           title="Add Todo"
           disabled={todo === ""}
         />
+      </View>
+      <View>
+        {todos.map((todo) => (
+          <Text key={todo.id}>{todo.title}</Text>
+        ))}
       </View>
     </View>
   );
